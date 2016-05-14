7bd98ba238aabd1710a77f8a89b8a94a425eb062
Nathan Walker <walkerrunpdx@gmail.com>
2016-05-13 17:59:09 -0700

    feat(Spotify): initial setup for spotify and @ngrx/store

--- src/app/eye-tunes.component.ts
+++ src/app/eye-tunes.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LogService } from './shared/index';
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
   moduleId: module.id,
   selector: 'eye-tunes-app',
   templateUrl: 'eye-tunes.component.html',
-  styleUrls: ['eye-tunes.component.css']
+  styleUrls: ['eye-tunes.component.css'],
+  directives: [SearchComponent]
 })
 export class EyeTunesAppComponent {
   title = 'eye-tunes works!';
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,1 +1,2 @@
 export * from './log.service';
+export * from './spotify.service';
